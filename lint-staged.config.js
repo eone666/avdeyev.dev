ce1681d60c975172e294a15aca7cb25f9395a59e
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-18 00:11:14 +0600

    check types before commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,7 @@ const micromatch = require("micromatch");
 const eslintConfig = require("./.eslintrc.json");
 
 module.exports = {
+  "*.{ts,tsx}": () => "npx tsc -p .",
   "*.{js,jsx,ts,tsx}": (files) => {
     const match = micromatch.not(files, eslintConfig.ignorePatterns, {
       matchBase: true,
