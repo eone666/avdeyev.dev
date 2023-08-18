c46ff853d83da2ee5aa872d0476ea88399e7f909
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-18 08:26:47 +0600

    connect font to tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "@/app/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const openSans = Open_Sans({
+  subsets: ["latin", "cyrillic"],
+  fallback: ["sans-serif"],
+  display: "swap",
+  variable: "--font-open-sans",
+  weight: ["400", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="font-open-sans">{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        "open-sans": "var(--font-open-sans)",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
