80f2f32513c062fcc5bf2bbb52f253f9eb7b5319
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-22 16:08:06 +0600

    background gradient

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,7 +4,7 @@ import ThemeChanger from "@/app/_components/theme-changer";
 
 export default function Header() {
   return (
-    <header className="fixed left-0 right-0 top-0 flex h-header items-center border-b bg-background">
+    <header className="fixed left-0 right-0 top-0 flex h-header items-center border-b">
       <Container className="flex items-center justify-between">
         <Logo />
         <nav className="flex items-center gap-4">
