708fcf3150ae9a1b72bbf6f413b989e2da493043
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-22 20:27:47 +0600

    add links to stack

--- app/(MainPage)/_components/stack.tsx
+++ app/(MainPage)/_components/stack.tsx
@@ -11,28 +11,28 @@ export default function Stack() {
   return (
     <ul className="flex gap-3 text-2xl">
       <li>
-        <Link href="#">
+        <Link href="https://www.typescriptlang.org/" target="_blank">
           <Icon>
             <SiTypescript />
           </Icon>
         </Link>
       </li>
       <li>
-        <Link href="#">
+        <Link href="https://react.dev/" target="_blank">
           <Icon>
             <SiReact />
           </Icon>
         </Link>
       </li>
       <li>
-        <Link href="#">
+        <Link href="https://nextjs.org/" target="_blank">
           <Icon>
             <SiNextdotjs />
           </Icon>
         </Link>
       </li>
       <li>
-        <Link href="#">
+        <Link href="https://tailwindcss.com/" target="_blank">
           <Icon>
             <SiTailwindcss />
           </Icon>
