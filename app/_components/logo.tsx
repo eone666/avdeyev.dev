871329d3b4744d42e56f86c040728f6f49bcbb8c
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-22 16:23:11 +0600

    logo animation

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="font-overpass-mono text-2xl">
-      IVAN_A<span className="hidden sm:inline">VDEYEV</span>
+      IVAN_A
+      <span className="hidden sm:inline">VDEYEV</span>
+      <span className="animate-ping">_</span>
     </Link>
   );
 }
