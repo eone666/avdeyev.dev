46b3a6e7e211e1c3d7b104f081ad31fb11fd3ac1
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-22 16:20:45 +0600

    new simple footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,35 +1,7 @@
-import Container from "@/components/ui/container";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
 export default function Footer() {
   return (
-    <footer className="flex h-footer items-center border-t bg-background">
-      <Container className="flex items-center justify-center">
-        <ul className="flex items-center gap-2">
-          <li>
-            <Button asChild variant="link">
-              <Link href="https://t.me/eone666" target="_blank">
-                Telegram
-              </Link>
-            </Button>
-          </li>
-          <li>
-            <Button asChild variant="link">
-              <Link href="https://www.linkedin.com/in/eone666/" target="_blank">
-                LinkedIn
-              </Link>
-            </Button>
-          </li>
-          <li>
-            <Button asChild variant="link">
-              <Link href="https://github.com/eone666" target="_blank">
-                GitHub
-              </Link>
-            </Button>
-          </li>
-        </ul>
-      </Container>
+    <footer className="flex h-footer items-center justify-center text-center">
+      Copyright © {new Date().getFullYear()} Ivan Avdeyev.
     </footer>
   );
 }
