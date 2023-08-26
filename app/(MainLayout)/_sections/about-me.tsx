50c61a1d2f69634d1c766f187d6dfa9141d86edd
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-26 19:46:05 +0600

    add image blur on load

--- app/(MainLayout)/_sections/about-me.tsx
+++ app/(MainLayout)/_sections/about-me.tsx
@@ -1,17 +1,19 @@
 import Section from "@/app/(MainLayout)/_components/section";
 import H2 from "@/components/ui/h2";
 import Image from "next/image";
+import photo from "@/public/my-photo.jpg";
 
 export default function AboutMe() {
   return (
     <Section className="mx-auto flex max-w-[1000px] flex-col items-center gap-2 pt-2 xs:gap-4 sm:justify-between md:flex-row md:gap-10">
       <div className="shrink-0">
         <Image
-          src="/my-photo.jpg"
+          src={photo}
           alt="my photo"
           width={300}
           height={300}
           className="h-[200px] w-[200px] md:h-[300px] md:w-[300px]"
+          placeholder="blur"
         />
       </div>
       <div>
