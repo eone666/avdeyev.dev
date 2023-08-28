09ddb97b489deb16dae2baf5116446631ae300a3
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-28 10:38:01 +0600

    fix zod

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 const zod = require("zod");
 
 const envSchema = zod.object({
-  ANALYZE: zod.string(),
+  ANALYZE: zod.enum(["true", "false"]).or(zod.undefined()),
 });
 
 module.exports = envSchema.parse(process.env);
