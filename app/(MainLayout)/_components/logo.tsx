98550a9f0b61bc1bf0084b5aea3e93a548782cdf
Ivan Avdeyev <eone.ukg@gmail.com>
2023-09-06 18:07:15 +0600

    change logo to "AVDEYEV.DEV"

--- app/(MainLayout)/_components/logo.tsx
+++ app/(MainLayout)/_components/logo.tsx
@@ -4,7 +4,7 @@ import Typewriter from "@/components/ui/typewriter";
 export default function Logo() {
   return (
     <Link href="/" className="font-overpass-mono text-xl sm:text-2xl">
-      <Typewriter text={"IVAN_AVDEYEV"} delay={100} />
+      <Typewriter text="AVDEYEV.DEV" delay={100} />
       <span className="animate-ping">_</span>
     </Link>
   );
